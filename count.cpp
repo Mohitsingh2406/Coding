96e9d9504387f7d81b934af3268a92ab6d53bf17
Mohit Singh Tomar <mohitsingh2004624@gmail.com>
2025-02-18 13:58:21 +0530

    CODE

---
+++ count.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int countDigits(int n){
+        int cnt =0;
+        while(n>0){
+            n /= 10;
+            cnt++;
+        }
+        return cnt;
+}
+
+int main() {
+    int n= 329823;
+    cout<<"N: "<<n<<endl;
+
+    int digits = countDigits(n);
+    cout<<"Number of digits: "<<digits<<endl;
+    return 0;
+}
