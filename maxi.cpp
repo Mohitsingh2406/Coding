28834ca63f0457762e41e6c050b2cf5fa10505c0
Mohit Singh Tomar <mohitsingh2004624@gmail.com>
2025-02-22 13:37:05 +0530

    FindMaximum

---
+++ maxi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+
+int main(){
+    int arr[]={2,3,4,5,6,7,81,2,4,91,56};
+    int size=11;
+
+    int maxi= INT_MIN;
+    for(int i=0;i<size;i++){
+        if(arr[i]>maxi){
+            maxi=arr[i];
+        }
+    }
+    cout<<"Maximum Number is:- "<<maxi<<endl;
+}
