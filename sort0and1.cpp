0c0df74e9edbc618f0949b5f967af214ac491ea7
Mohit Singh Tomar <mohitsingh2004624@gmail.com>
2025-03-07 18:07:58 +0530

    Sort0'sand1's

---
+++ sort0and1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> arr{0,0,1,0,1,1,0,0,0,1,1,0,1,};
+
+    int start =0;
+    int end = arr.size()-1;
+    int i =0;
+
+    while(start<=end){
+        if(arr[start] == 0){
+            swap(arr[start],arr[i]);
+            i++;
+            start++;
+        }
+        else{
+            swap(arr[i],arr[end]);
+            end--;
+        }
+    }
+
+    for(auto value : arr){
+        cout<<value<<" ";
+    }
+}
